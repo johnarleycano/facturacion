f20081505ac642697c67c817f414b83661b5bee6
John Arley <johnarleycano@hotmail.com>
2019-01-29 08:31:11 -0500

    feat(Config): eliminado index.php de la url

--- application/config/production/config.php
+++ application/config/production/config.php
@@ -47,7 +47,7 @@ $config['base_url'] = 'https://facturacion.devimed.com.co/';
 | variable so that it is blank.
 |
 */
-$config['index_page'] = 'index.php';
+$config['index_page'] = '';
 
 /*
 |--------------------------------------------------------------------------
